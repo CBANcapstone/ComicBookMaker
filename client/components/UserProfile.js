6afc4d5f185dbf9fe9cf281b3e9ec507eba65fd5
Mykola Maslov <kategorychnost@gmail.com>
2018-06-28 17:40:35 -0400

    user buttons

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import ThumbnailsGrid from './ThumbnailsGrid';
-import { getUserStoriesThunk } from '../store'
-
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import ThumbnailsGrid from './ThumbnailsGrid'
+import {getUserStoriesThunk} from '../store'
 
 class UserProfile extends Component {
   componentDidMount() {
     this.props.getUserStories(this.props.user.id)
   }
 
+  filterCreated() {
+    console.log('created')
+
+  }
+  filterContributed() {
+    console.log('contributed')
+    // stories = this.props.stories.filter(story=>story)
+  }
+
   render() {
     // console.log(this.props)
     // console.log('(#_#) ',this.state)
 
-    const { name, email, photoUrl } = this.props.user;
-    // const {stories} = this.props.stories
+    const {name, email, photoUrl} = this.props.user
+    const {stories} = this.props
     return (
       <div className="profile-container">
         <div className="profile-info-container">
@@ -29,15 +37,15 @@ class UserProfile extends Component {
         </div>
 
         <div className="profile-filter-bar">
-          <button>created</button>
-          <button>contributed</button>
+          <button onClick={this.filterCreated}>created</button>
+          <button onClick={this.filterContributed}>contributed</button>
         </div>
 
         <div className="profile-stories-lst">
-          <ThumbnailsGrid list={this.props.stories} />
+          <ThumbnailsGrid list={stories} />
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -45,13 +53,13 @@ function mapState(state) {
   return {
     user: state.user,
     stories: state.story
-  };
+  }
 }
 
 function mapDispatch(dispatch) {
   return {
-    getUserStories: (id) => dispatch(getUserStoriesThunk(id))
-  };
+    getUserStories: id => dispatch(getUserStoriesThunk(id))
+  }
 }
 
-export default connect(mapState, mapDispatch)(UserProfile);
+export default connect(mapState, mapDispatch)(UserProfile)
