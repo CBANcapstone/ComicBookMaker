9d488dffd776aed8cdaf7f0d22989037c9813fa4
Bushra Taimoor <bushra376@hotmail.com>
2018-06-29 17:00:06 -0400

    remove error on user profile

--- server/api/index.js
+++ server/api/index.js
@@ -1,22 +1,22 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
 router.use('/', (req, res, next) => {
-  // if (!req.user) {
-  //   const error = new Error('Unauthorized. Please login to see that page.')
-  //   error.status = 401
-  //   next(error)
-  // } else {
-    next()
-  // }
-})
+  if (!req.user) {
+    const error = new Error('Unauthorized. Please login to see that page.');
+    error.status = 401;
+    next(error);
+  } else {
+    next();
+  }
+});
 
-router.use('/users', require('./users'))
-router.use('/templates', require('./templates'))
-router.use('/stories', require('./stories'))
+router.use('/users', require('./users'));
+router.use('/templates', require('./templates'));
+router.use('/stories', require('./stories'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
