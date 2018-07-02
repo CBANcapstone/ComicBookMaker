c2fa5efb86dfea9703ef495aecde153af3279549
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-07-02 17:39:05 -0400

    Modal Styled

--- client/components/SingleStory.js
+++ client/components/SingleStory.js
@@ -4,17 +4,6 @@ import { Link } from 'react-router-dom';
 import { fetchStoryThunk } from '../store';
 import Modal from 'react-modal';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
-
 Modal.setAppElement('#app');
 
 class SingleStory extends Component {
@@ -26,18 +15,12 @@ class SingleStory extends Component {
     };
 
     this.openModal = this.openModal.bind(this);
-    // this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
   openModal(imageUrl) {
     this.setState({ modalIsOpen: true, imageUrl });
   }
-
-  // afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   this.subtitle.style.color = '#f00';
-  // }
 
   closeModal() {
     this.setState({ modalIsOpen: false });
@@ -49,52 +32,52 @@ class SingleStory extends Component {
   render() {
     const { coverImgUrl, title, chapters } = this.props.story;
     const { creator, contributors } = this.props;
-    console.log('STATE', this.state.imageUrl);
     return (
       <div>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          // style={customStyles}
-          contentLabel="Example Modal"
-          className="Modal"
-          overlayClassName="Overlay"
-        >
-          {/* <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2> */}
-          <button
-            className="grid-thumbnail-btn "
-            type="button"
-            onClick={this.closeModal}
-          >
-            <span>CLOSE</span>
-          </button>
-
-          <img className="modal-image" src={this.state.imageUrl} alt="image" />
-        </Modal>
-
-        <div className="single-story-info">
-          <img
-            className="single-story-info-img"
-            src={coverImgUrl}
-            alt="cover"
-          />
-          <div className="single-story-info-title">
-            <div>Story name: {title}</div>
-            <div>Creator: {creator && creator.email}</div>
-            <div>
-              Contributors:{' '}
-              {contributors &&
-                contributors.map(col => {
-                  return <p key={col.id}>{col.email}</p>;
-                })}
-            </div>
-            <button type="button" className="single-story-btn">
-              <span>See completed chapters</span>
-            </button>
-          </div>
-        </div>
         <div className="single-story-container">
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            contentLabel="Example Modal"
+            className="Modal"
+            overlayClassName="Overlay"
+          >
+            <button
+              className="grid-thumbnail-btn "
+              type="button"
+              onClick={this.closeModal}
+            >
+              <span>CLOSE</span>
+            </button>
+            <img
+              className="modal-image"
+              src={this.state.imageUrl}
+              alt="image"
+            />
+          </Modal>
+
+          <div className="single-story-info">
+            <img
+              className="single-story-info-img"
+              src={coverImgUrl}
+              alt="cover"
+            />
+            <div className="single-story-info-title">
+              <div>Story name: {title}</div>
+              <div>Creator: {creator && creator.email}</div>
+              <div>
+                Contributors:{' '}
+                {contributors &&
+                  contributors.map(col => {
+                    return <p key={col.id}>{col.email}</p>;
+                  })}
+              </div>
+              <button type="button" className="single-story-btn">
+                <span>View The Story</span>
+              </button>
+            </div>
+          </div>
           <div className="single-story-chapters">
             <h3 className="center-item-heading">
               Select the Chapter You Want To Create
@@ -179,8 +162,5 @@ function mapDispatch(dispatch, ownProps) {
 
 export default connect(mapState, mapDispatch)(SingleStory);
 
-// Still load all chapter : completed and open
-// If chapter is completed => pic is on background of the box and BUTTON is VIEW not EDIT
-// If you click => modal with picture / fullscreen
 // Button 'see completed chaps => view the story => new component
 // ***extra*** edit story name / cover image => modal
