70f34689cf5b6b60572599fd2941c1d550c29b2f
Bushra Taimoor <bushra376@hotmail.com>
2018-07-03 14:07:15 -0400

    see completed story slide show

--- client/components/CompletedChapter.js
+++ client/components/CompletedChapter.js
@@ -11,20 +11,60 @@ class CompletedChapter extends Component {
 
   render() {
     const { title } = this.props.story;
+    const { completedChapters } = this.props;
     return (
-      <div className="single-story-completed-chapters">
-        <h1> {title} </h1>
-        {this.props.completedChapters.map(chapter => {
-          return (
-            <div
-              className="single-story-completed-chapter-img"
-              key={chapter.id}
-            >
-              <h3>{chapter.title}</h3>
-              <img src={chapter.imageUrl} width="224" />
-            </div>
-          );
-        })}
+      <div className="completed-chapters">
+        <div className="completed-chapters-header">
+          <h1>Story - {title} </h1>
+        </div>
+        <ul className="slides">
+          {completedChapters.map((chapter, idx) => {
+            let prevPicNum = idx === 0 ? completedChapters.length - 1 : idx;
+            let nextPicNum = idx === completedChapters.length - 1 ? 1 : idx + 2;
+            console.log('imageurl', chapter.imageUrl);
+            return (
+              <div
+                className="single-story-completed-chapter-img"
+                key={chapter.id}
+              >
+                <input
+                  type="radio"
+                  name="radio-btn"
+                  id={`img-${idx + 1}`}
+                  checked
+                />
+                <li className="slide-container">
+                  <div className="slide">
+                    {!chapter.imageUrl ? (
+                      <div className="no-image-div">{chapter.title}</div>
+                    ) : (
+                      <img src={chapter.imageUrl} width="224" />
+                    )}
+                  </div>
+
+                  <div className="nav">
+                    <label htmlFor={`img-${prevPicNum}`} className="prev">
+                      &#x2039;
+                    </label>
+                    <label htmlFor={`img-${nextPicNum}`} className="next">
+                      &#x203a;
+                    </label>
+                  </div>
+                </li>
+              </div>
+            );
+          })}
+          <li className="nav-dots">
+            {completedChapters.map((ele, idx) => (
+              <label
+                htmlFor={`img-${idx + 1}`}
+                className="nav-dot"
+                id={`img-dot-${idx + 1}`}
+                key={ele.title + idx}
+              />
+            ))}
+          </li>
+        </ul>
       </div>
     );
   }
