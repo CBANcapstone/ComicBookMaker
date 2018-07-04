fd8ea7178707523cb2651ec7f0abc4e46bb49f2a
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-07-04 01:28:22 -0400

    CSS Structured

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import { Router } from 'react-router-dom';
 import history from './history';
 import store from './store';
 import App from './App';
+import css from './css/index.css'
 
 import './socket';
 
