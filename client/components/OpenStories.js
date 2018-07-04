a14ce5aa0e71cf0fdf8bcc3fc8a1bc86fb9576e4
Bushra Taimoor <bushra376@hotmail.com>
2018-07-04 00:08:30 -0400

    navbar has create new story link, open stories is edited, route to emplates after log in

--- client/components/OpenStories.js
+++ client/components/OpenStories.js
@@ -3,40 +3,41 @@ import { connect } from 'react-redux';
 import { UnitStory } from '/';
 import { fetchOpenStoriesThunk } from '../store';
 
-class OpenStories extends Component{
-
-  constructor(){
+class OpenStories extends Component {
+  constructor() {
     super();
   }
-  componentDidMount(){
+  componentDidMount() {
     this.props.getOpenStories();
   }
 
-  render(){
-    return(
-      <div className='open-stories'>
-        {this.props.openStories ? this.props.openStories.map(story => {
-                                    return <UnitStory story={story} />
-                                  }): <h1>There are no open stories yet.</h1>
-        }
-  }
+  render() {
+    return (
+      <div className="open-stories">
+        {this.props.openStories ? (
+          this.props.openStories.map(story => {
+            return <UnitStory key={story.id} story={story} />;
+          })
+        ) : (
+          <h1>There are no open stories yet.</h1>
+        )}
       </div>
     );
   }
 }
 
-const mapToState = (state) => {
-  return{
+const mapToState = state => {
+  return {
     openStories: state.stories
-  }
-}
+  };
+};
 
-const mapDispatch = (dispatch) => {
-  return{
+const mapDispatch = dispatch => {
+  return {
     getOpenStories: () => {
-      dispatch(fetchOpenStoriesThunk())
+      dispatch(fetchOpenStoriesThunk());
     }
-  }
-}
+  };
+};
 
 export default connect(mapToState, mapDispatch)(OpenStories);
